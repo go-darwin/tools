d247159493e83c12521ff82f99124619511a2460
Koichi Shiraishi <zchee.io@gmail.com>
2021-06-12 09:05:44 +0900

    cmd/asmvet: remove darwin build tag
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- cmd/asmvet/main.go
+++ cmd/asmvet/main.go
@@ -1,9 +1,6 @@
 // SPDX-FileCopyrightText: Copyright 2021 The go-darwin Authors
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:build darwin && amd64 && gc
-// +build darwin,amd64,gc
-
 // Command asmvet checks for correctness of Go assembly.
 //
 // Standalone version of the assembly checks in go vet.
