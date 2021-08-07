ca266c714de6a674403b5c3318a2fbeac86809ff
Koichi Shiraishi <zchee.io@gmail.com>
2021-08-07 12:25:30 +0900

    cmd/asmvet: add atomic,atomicalign and unsafeptr analyzers
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- cmd/asmvet/main.go
+++ cmd/asmvet/main.go
@@ -1,7 +1,7 @@
-// SPDX-FileCopyrightText: 2021 The go-darwin Authors
+// SPDX-FileCopyrightText: 2021 The Go Darwin Authors
 // SPDX-License-Identifier: BSD-3-Clause
 
-// Command asmvet checks for correctness of Go assembly.
+// Command asmvet checks for correctness of Go assembly and some low-level operations.
 //
 // Standalone version of the assembly checks in go vet.
 package main
@@ -10,12 +10,18 @@ import (
 	"golang.org/x/tools/go/analysis/unitchecker"
 
 	"golang.org/x/tools/go/analysis/passes/asmdecl"
+	"golang.org/x/tools/go/analysis/passes/atomic"
+	"golang.org/x/tools/go/analysis/passes/atomicalign"
 	"golang.org/x/tools/go/analysis/passes/framepointer"
+	"golang.org/x/tools/go/analysis/passes/unsafeptr"
 )
 
 func main() {
 	unitchecker.Main(
 		asmdecl.Analyzer,
+		atomic.Analyzer,
+		atomicalign.Analyzer,
 		framepointer.Analyzer,
+		unsafeptr.Analyzer,
 	)
 }
